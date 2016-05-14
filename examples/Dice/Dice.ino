2767813c7213ef0f370deca4723b0b2731a009e1
Martin Maly <maly@maly.cz>
2016-05-14 15:38:29 +0200

    Dice example

---
+++ examples/Dice/Dice.ino
@@ -0,0 +1,30 @@
+#include <Wire.h>
+
+#include <edu101.h>
+
+
+int BUTTON = 2;
+
+void setup() {
+  pinMode(BUTTON,INPUT_PULLUP);
+  //delay(1000);
+  Display.set(0,0);
+  Display.set(0,1);
+  Display.set(0,2);
+}
+
+int num = 1;
+
+void loop() {
+  Display.num(num,3);
+
+  int b = digitalRead(BUTTON);
+  
+  if (!b) {
+    num++;
+    if (num==7) num=1;
+  } 
+    delay(10);
+
+
+}
