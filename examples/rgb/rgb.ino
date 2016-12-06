03dab1f4b13aecec697fb27d3294a146fa623532
Martin Maly <maly@maly.cz>
2016-12-06 11:17:48 +0100

    RGB fixed

--- examples/rgb/rgb.ino
+++ examples/rgb/rgb.ino
@@ -1,6 +1,6 @@
-int RED = 6;
+int RED = 9;
 int GREEN = 5;
-int BLUE = 9;
+int BLUE = 6;
 
 void setup() {
   // put your setup code here, to run once:
@@ -16,13 +16,13 @@ void loop() {
   // put your main code here, to run repeatedly:
   digitalWrite(RED,LOW);
   delay(500);
-  digitalWrite(GREEN,LOW);
-  delay(500);
-  digitalWrite(BLUE,LOW);
-  delay(500);
   digitalWrite(RED, HIGH);
   delay(500);
+  digitalWrite(GREEN,LOW);
+  delay(500);
   digitalWrite(GREEN, HIGH);
+  delay(500);
+  digitalWrite(BLUE,LOW);
   delay(500);
   digitalWrite(BLUE, HIGH);
   delay(500);
