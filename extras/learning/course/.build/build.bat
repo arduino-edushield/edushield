7079f021f1f884ba5a95b99fa2f1834a3c8050f0
Martin Maly <maly@maly.cz>
2017-02-09 12:39:34 +0100

    Path fixed, some minor changes

--- extras/learning/course/.build/build.bat
+++ extras/learning/course/.build/build.bat
@@ -1,3 +1,3 @@
 cd ..\cz\
-copy readme.md+arduino-sw-hw.md+blink.md+semafor.md+rgb.md+serial.md+displej.md+lightscale.md+temperature.md+rtc.md+interrupt.md+fridge.md+random.md+ethernet.md+datalogger.md+cheatsheet.md czech-full.md
+copy readme.md+uvod.md+arduino-sw-hw.md+blink.md+semafor.md+rgb.md+serial.md+displej.md+lightscale.md+temperature.md+rtc.md+interrupt.md+fridge.md+random.md+ethernet.md+datalogger.md+cheatsheet.md czech-full.md
 cd ..\.build
